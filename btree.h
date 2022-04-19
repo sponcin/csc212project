e1b46fbb3c29aa5a693ad105337c336fec8a0e0e
sponcin <78090542+sponcin@users.noreply.github.com>
2022-04-19 14:32:42 -0400

    Add files via upload
    insertNonFull recursive call is throwing a seg fault 11.
    data, isLeaf, currentKeys and children were moved from private to public. Under private they weren't able to be accessed.

--- btree.h
+++ btree.h
@@ -5,28 +5,37 @@
 class BTNode
 {
       private:
-        //child pointer
-        BTNode* child;
-        
-    public:
-        struct wordData 
+
+        //struct containing the word, its resultant key, and a counter for duplicates
+        struct wordData
         {
             std::string word;
-            int hash;
-            int ascii;
-            int count;
+            int key;
+            int count = 0;
         };
-        //how many spaces available in a vector (minus 1)
-        int key; 
-        //vector using wordData data type
-        std::vector<wordData> data; 
+
+        //minimum degree of the b tree
+        //minimum degree:a fixed integer t >= 2. Every node other than the root must have at
+        //least t-1 keys. Every internal node other than the root thus has at least t children.
+        int minDeg;
+
+
+    public:
         BTNode();
-        BTNode(std::string word, int key);
-        int hashFunction(std::string word);
+        BTNode(int minDegree_, bool leafy);
+
+    //changed the follow 4 variables from private to public
+        //vector of type wordData
+        std::vector<wordData> data;
+        //boolean to determine if the node is a leaf node
+        bool isLeaf;
+        int currentKeys;
+        //child pointer
+        std::vector<BTNode*> children;
 
         ~BTNode();
 
-    friend class BTree; //can access RBTNode in the RBTree class
+friend class BTree; //can access RBTNode in the RBTree class
 
 };
 
@@ -35,22 +44,19 @@ class BTree
     private:
         BTNode* root;
 
-        BTNode* insert(int data, BTNode* root);
-
-        bool search(int data, BTNode* root);
-
-        BTNode* splitChild(int data, BTNode* root);
+        int minDeg;
 
     public:
-        BTree();
+        BTree(int minDeg_);
         ~BTree();
 
-        void insert(int data);
+        void insert(int theKey, std::string theWord);
+        void splitChild(int index, BTNode *parent, BTNode *toSplit);
+        void insertNonFull(BTNode *node, int theKey, std::string theWord);
+        bool search(int key, BTNode* root);
+        void searchWhenInput(int key, BTNode* root, bool &isPresent);
+        void callSearch(int key, bool &isPresent);
 
-        bool search(int data);
+friend class BTNode;
 
-        void splitChild(int data);
-
-    //data_size = size of the vectors
-    //children size = data_size + 1
 };
--- main.cpp
+++ main.cpp
@@ -3,69 +3,63 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <string>
+#include <stdlib.h>
 
-//function declaration to read in the txt file
-void ReadFile(std::string input_file_name, std::vector<std::string>* data);
+int hashFunction(std::string word);
 
 //main function that takes in two parameters
 int main(int argc, char*argv[])
 {
-    //command line info at index 1 is the file name that the user is trying to open
-    std::string input_file_name = std::string(argv[1]);
 
-    //command line info at index 2 is the size of each child's vector
-    std::string k = std::string(argv[2]);
+    std::string word;
 
-    //initialize the string vector to hold read information from the txt file
-    std::vector<std::string> data;
+    std::string inputF = std::string(argv[1]);
 
-    //debugging/printing file name
-    std::cout << input_file_name << std::endl;
+    std::ifstream input_file(inputF);
 
-    //debugging/printing k
-    std::cout << k << std::endl;
-
-    //function call to read the file and saves words to data vector
-    ReadFile(input_file_name, &data);
-
-    //gets size of data vector
-    int size = data.size();
-
-    //debugging/print size of data - currently keeps printing 0?
-    std::cout << size << std::endl;
-
-    //debugging/loops through and prints out data at all indices
-    for(int i = 0; i < size; i++)
-    {
-        std::cout << data[i] << std::endl;
-    }
+    char* end;
     
+    int b = 10;
+
+    long int m = strtol(argv[2], &end, b);
+
+    BTree myTree(m);
+
+    while (input_file >> word) {
+
+        if (word[word.length() - 1] == ','||'.'||'?'||':'||'"'||'!'||';') {
+            word.pop_back();
+        }
+
+        if (word[0] == '"') {
+            word.erase(0,1);
+        }
+
+        int key = hashFunction(word);
+
+        bool isPresent;
+
+        myTree.callSearch(key, isPresent);
+
+// this code is throwing seg fault 11
+        if (!isPresent) {
+            myTree.insert(key, word);
+        }
+    }
+
+    input_file.close();
+
 }
 
-//Read from a file
-void ReadFile(std::string input_file_name, std::vector<std::string>* data){
-    // Create the input filestream - opens the file & prepares it for reading
-    std::ifstream file(input_file_name);
 
-    // Creates a temporary vector to represent one row
-    std::vector<std::string> new_row;
+int hashFunction(std::string word) {
+    int key = 0;
 
-    // Temporary string to hold a single line of the file
-    std::string str;
-
-    // Reads all lines in the file, 1 at at time
-    while (std::getline(file, str)) 
-    {
-      // Converts our string double to a stringstream
-      std::istringstream ss(str);
-      // Temp double to store a converted value from a line
-      std::string token;
-
-        // Reads all values from the stringstream (current row), converts to double
-        while(ss >> token){
-            // Adds the converted value to the row
-            new_row.push_back(token);
-        }
-        new_row.clear();
+    for (int i = 0; i < word.length(); i++) {
+        int ascii = int(word[i]) * (i + 1);
+        key += ascii;
     }
+
+    return key;
 }
