b75f1271d27c9f60799dee2d3d7b79a3bb544e1f
sponcin <78090542+sponcin@users.noreply.github.com>
2022-04-23 16:59:26 -0400

    Add files via upload
    final without cleaning comments

--- btree.h
+++ btree.h
@@ -1,56 +1,70 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <iostream>
 
 class BTNode
 {
-      private:
+      public:
         //child pointer
-        BTNode* child;
-        
-    public:
-        struct wordData 
+        std::vector<BTNode*> children;
+
+        //struct containing the word, its resultant key, and a counter for duplicates
+        struct wordData
         {
             std::string word;
-            int hash;
-            int ascii;
-            int count;
+            int key;
+            int count = 0;
         };
-        //how many spaces available in a vector (minus 1)
-        int key; 
-        //vector using wordData data type
-        std::vector<wordData> data; 
+
+        //minimum degree of the b tree
+        //minimum degree:a fixed integer t >= 2. Every node other than the root must have at
+        //least t-1 keys. Every internal node other than the root thus has at least t children.
+        int minDeg;
+
+        //current number of key, word pairs in the data vector
+        int currentKeys;
+
+        int id;
+
+        //vector of type wordData
+        std::vector<wordData> data;
+
+        //boolean to determine if the node is a leaf node
+        bool isLeaf;
+
         BTNode();
-        BTNode(std::string word, int key);
-        int hashFunction(std::string word);
+        BTNode(int minDegree_, bool leafy);
 
         ~BTNode();
-
-    friend class BTree; //can access RBTNode in the RBTree class
-
 };
 
 class BTree
 {
-    private:
-        BTNode* root;
-
-        BTNode* insert(int data, BTNode* root);
-
-        bool search(int data, BTNode* root);
-
-        BTNode* splitChild(int data, BTNode* root);
 
     public:
+        BTNode* root;
+
+        int idCounter = 0;
+
+        int minDeg;
+
         BTree();
+        BTree(int minDeg_);
         ~BTree();
 
-        void insert(int data);
-
-        bool search(int data);
-
-        void splitChild(int data);
-
-    //data_size = size of the vectors
-    //children size = data_size + 1
+        void insert(int theKey, std::string theWord);
+        void splitChild(int index, BTNode *parent, BTNode *toSplit);
+        void insertNonFull(BTNode *node, int theKey, std::string theWord);
+        bool search(int key, BTNode* root);
+        void searchWhenInput(int key, BTNode* root, bool &isPresent);
+        void callSearch(int key, bool &isPresent);
+        void printNode(BTNode* node);
+        void traverse(BTNode* node);
+        void callTraverse();
+        void printdot(std::string file_name);
+        // void recursdot(BTNode* node,  std::ofstream &output_file); 
+        void traverseDot(BTNode* node, std::ofstream &output_file);
+        void assignID(BTNode* node);
+        void assign();
 };
