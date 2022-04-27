921e7477d48b06d2e0bae7d32b69c63e66447ebe
bvanderlaan98 <78108372+bvanderlaan98@users.noreply.github.com>
2022-04-27 19:11:52 -0400

    Final code
    Adjusted main to continually accept words from the user until they type "0", which then stops the program.

--- main.cpp
+++ main.cpp
@@ -3,69 +3,153 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <string>
+#include <stdlib.h>
+#include <algorithm>
 
-//function declaration to read in the txt file
-void ReadFile(std::string input_file_name, std::vector<std::string>* data);
+int hashFunction(std::string word);
 
-//main function that takes in two parameters
+//main function that takes in three parameters
 int main(int argc, char*argv[])
 {
-    //command line info at index 1 is the file name that the user is trying to open
-    std::string input_file_name = std::string(argv[1]);
 
-    //command line info at index 2 is the size of each child's vector
-    std::string k = std::string(argv[2]);
+    //string that will be updated with each word in the file
+    std::string word;
 
-    //initialize the string vector to hold read information from the txt file
-    std::vector<std::string> data;
+    //name of input file
+    std::string inputF = std::string(argv[1]);
 
-    //debugging/printing file name
-    std::cout << input_file_name << std::endl;
+    //name of output file
+    std::string outputF = std::string(argv[2]);
 
-    //debugging/printing k
-    std::cout << k << std::endl;
+    //create input stream for while loop
+    std::ifstream input_file(inputF);
 
-    //function call to read the file and saves words to data vector
-    ReadFile(input_file_name, &data);
+    //initialize integer variable to count the number of sentences
+    int sentenceCount = 0;
 
-    //gets size of data vector
-    int size = data.size();
+    //mimimum degree, see btree.h for explanation
+    int minDeg = std::stoi(argv[3]);
 
-    //debugging/print size of data - currently keeps printing 0?
-    std::cout << size << std::endl;
+    //initialize the btree using minimum degree
+    BTree myTree(minDeg);
 
-    //debugging/loops through and prints out data at all indices
-    for(int i = 0; i < size; i++)
-    {
-        std::cout << data[i] << std::endl;
+    //while the input stream receives words
+    while (input_file >> word) {
+
+         //if the current word ends with any of the following punctuation, increment sentenceCount
+         if (word[word.length() - 1] =='.'|| word[word.length() - 1] =='?'|| word[word.length() - 1] =='!' || word[word.length() - 1] == '"') {
+            sentenceCount += 1;
+        }
+
+        //loop through word, eliminate all punctuation except for apostrophes
+        for (int i = word.size(); i >= 0; i--) {
+            if (std::ispunct(word[i]) || word[i] == '"') {
+                word.erase(i,1);
+            }
+        }
+
+
+        //make the word all lowercase
+        std::for_each(word.begin(),word.end(), [](char &letter) {
+            letter = std::tolower(letter);
+        });
+
+        //hash the word
+        int key = hashFunction(word);
+
+        //boolean used to determine if the word is present in the tree or not
+        bool isPresent;
+
+        //search what already exists in the tree. if present, count is updated
+        myTree.callSearch(key,isPresent);
+
+        //if the word is not present in the tree, insert it
+        if (!isPresent) {
+            myTree.insert(key,word);
+        }
     }
-    
+
+    //close the file
+    input_file.close();
+
+    //traverse and print the contents of each node to the terminal
+    myTree.callTraverse();
+
+    //assign each node a number for the dot file
+    myTree.assign();
+
+    //create output file for dot format representation
+    myTree.printdot(outputF);
+
+    //output number of sentences in the text file
+    std::cout << "This text file has " << sentenceCount << " sentences." << std::endl;
+
+    myTree.callPopular();
+
+    myTree.findAvg();
+
+    std::cout << "Would you like to search for a word in this file? Answer Yes or No" << std::endl;
+
+    std::string response;
+
+    std::cin >> response;
+
+    if (response == "Yes") {
+        //prompt user for word
+        std::cout << "Please type the word you want to find. Type 0 to stop searching." << std::endl;
+
+        std::string userSearch;
+
+        //do search
+        while (std::cin >> userSearch) {
+
+            if (userSearch == "0") {
+                break;
+            }
+
+            //stip word of punctuation, change all uppercase letters to lower
+            for (int i = userSearch.size(); i >= 0; i--) {
+                if (std::ispunct(userSearch[i]) || userSearch[i] == '"') {
+                    userSearch.erase(i,1);
+                }
+            }
+
+            std::for_each(userSearch.begin(),userSearch.end(), [](char &letter) {
+                letter = std::tolower(letter);
+            });
+
+            //generate key
+            int userKey = hashFunction(userSearch);
+
+            int myCount = 0;
+            bool isWordPresent;
+
+            myTree.callUserSearch(userKey,isWordPresent,myCount);
+
+            if (isWordPresent) {
+                std::cout << "The word " << userSearch << " is in this text. It appears " << myCount << " times." << std::endl;
+            } else {
+                std::cout <<  "The word " << userSearch << " is not in this text." << std::endl;
+            }
+        }
+
+    } else {
+        std::cout << "Program end." << std::endl;
+        return 0;
+    }
+
+    std::cout << "Program end." << std::endl;
+    return 0;
+
 }
 
-//Read from a file
-void ReadFile(std::string input_file_name, std::vector<std::string>* data){
-    // Create the input filestream - opens the file & prepares it for reading
-    std::ifstream file(input_file_name);
 
-    // Creates a temporary vector to represent one row
-    std::vector<std::string> new_row;
+int hashFunction(std::string word) {
+    //get hash value of each word
+    std::hash<std::string> hash;
+    int key = hash(word);
 
-    // Temporary string to hold a single line of the file
-    std::string str;
-
-    // Reads all lines in the file, 1 at at time
-    while (std::getline(file, str)) 
-    {
-      // Converts our string double to a stringstream
-      std::istringstream ss(str);
-      // Temp double to store a converted value from a line
-      std::string token;
-
-        // Reads all values from the stringstream (current row), converts to double
-        while(ss >> token){
-            // Adds the converted value to the row
-            new_row.push_back(token);
-        }
-        new_row.clear();
-    }
+    //return key
+    return key;
 }
